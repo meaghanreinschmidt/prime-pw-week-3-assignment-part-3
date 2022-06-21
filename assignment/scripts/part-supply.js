d3db6cad17d50d36a48c3f50206197cdc279868a
Meaghan Reinschmidt <mreinschmidt@hotmail.com>
2022-06-21 13:45:52 -0500

    answered question 7 and added CSS background

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -57,6 +57,18 @@ console.log('---  Stretch Goals  ---');
 // 7. Rewrite the `for` loop from #6 as a `for of` loop. 
 console.log('7. Showing supplyChanges with "for of" loop');
 
+for(let parts of supplyChanges) {
+    if  (parts > 0) {
+        console.log('Added ' + parts + ' parts.');
+    } 
+    if (parts === 0) {
+        console.log('No Change.');
+    }
+    if (parts < 0) {
+        console.log('Removed ' + parts + ' parts.');
+    }
+}
+
 // 8. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
 console.log('8. Total supplies available is:');
